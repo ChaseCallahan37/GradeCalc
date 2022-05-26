95282ec94db981cccfa05cc485a3efc329d54d0f
Luke Dale <lhdale@crimson.ua.edu>
2022-05-26 13:18:32 -0500

    profdevone

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,13 @@
-const catDiv = document.querySelector("#category")
-const catNameTxt = document.querySelector("#category-name")
-const catWeightTxt = document.querySelector("#category-weight")
-const addCatBtn = document.querySelector("#add-category")
-const addCatForm = document.querySelector("#add-category-form")
+const calcGrade = document.querySelector('#calculate-grade')
+const aimsEarned = document.querySelector('#aims-earned')
+const aimsTotal = document.querySelector('#aims-total')
 
-let allCats = []
+const calculateGrade = function(a, b) {
+    let earned = a.value
+    let total = b.value
+    console.log((earned / total) * 100)
+}
 
-addCatForm.addEventListener("submit", (e)=>{
-    e.preventDefault()
-    const catName = e.target[0].value
-    const catWeight = e.target[1].value
-
-    const newDiv = document.createElement("div")
-    const categoryNameEl = document.createElement("label")
-    const addGradeBtn = document.createElement("button")
-
-    addGradeBtn.textContent = "+"
-    categoryNameEl.textContent = catName
-
-    newDiv.appendChild(categoryNameEl)
-    newDiv.appendChild(addGradeBtn)
-
-    e.target.appendChild(newDiv)
-
-    // const newCat = new Category(catName, catWeight)
-    
-    // const 
-    // console.log(e.target.document.querySelector("#category-name"))
-    // e.target.catWeightTxt
+window.addEventListener("keypress", (e) => {
+    calculateGrade(a, b)
 })
