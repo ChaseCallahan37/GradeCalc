8970a0c75baa92c994afcaa3eddbea096b0e2471
Your Name <you@example.com>
2022-05-28 03:43:21 -0500

    test if works

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,13 @@
-//Prof Dev Elements
+const calcGrade = document.querySelector("#calculate-grade");
 const aimsEarned = document.querySelector("#aims-earned");
 const aimsTotal = document.querySelector("#aims-total");
-const otherEarned = document.querySelector("#other-earned");
-const otherTotal = document.querySelector("#other-total");
 
-//Lab/Quiz/Part Elements
-const labEarned = document.querySelector("#labs-earned");
-const labTotal = document.querySelector("#labs-total");
-const quizEarned = document.querySelector("#quiz-earned");
-const quizTotal = document.querySelector("#quiz-total");
-const parEarned = document.querySelector("#parLab-earned");
-const parTotal = document.querySelector("#parLab-total");
+const calculateGrade = function (a, b) {
+  let earned = a.value;
+  let total = b.value;
+  console.log((earned / total) * 100);
+};
 
-let allGrades = pullGradesFromStor();
-
-//Event listener that will be called anytime there is a mouse click.
-//Will pull the data from all input boxes and render the page
-window.addEventListener("click", (e) => {
-  pullAllCats();
-  render();
+window.addEventListener("keypress", (e) => {
+  calculateGrade(a, b);
 });
-
-render();
