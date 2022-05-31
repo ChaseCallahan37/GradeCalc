1c6ee0eae2f507c1ea40c510ea36493010f4f63f
Riva Sword <rbsword@crimson.ua.edu>
2022-05-31 13:04:45 -0500

    exams complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,27 @@
-const calcGrade = document.querySelector("#calculate-grade");
+//Prof Dev inputs
 const aimsEarned = document.querySelector("#aims-earned");
 const aimsTotal = document.querySelector("#aims-total");
+const otherEarned = document.querySelector("#other-earned");
+const otherTotal = document.querySelector("#other-total");
 
-const calculateGrade = function (a, b) {
-  let earned = a.value;
-  let total = b.value;
-  console.log((earned / total) * 100);
-};
+//Labs/Quiz/Part
+const labEarned = document.querySelector("#labs-earned");
+const labTotal = document.querySelector("#labs-total");
+const quizEarned = document.querySelector("#quiz-earned");
+const quizTotal = document.querySelector("#quiz-total");
+const parEarned = document.querySelector("#parLab-earned");
+const parTotal = document.querySelector("#parLab-total");
+
+//Exams
+const exam1Earned = document.querySelector('exam1-earned');
+const exam2Earned = document.querySelector('exam2-earned');
+const exam3Earned = document.querySelector('exam3-earned');
+
+
+let allGrades = pullGradesFromStor();
 
 window.addEventListener("keypress", (e) => {
-  calculateGrade(a, b);
+  pullAllCats();
+  render();
 });
+render();
